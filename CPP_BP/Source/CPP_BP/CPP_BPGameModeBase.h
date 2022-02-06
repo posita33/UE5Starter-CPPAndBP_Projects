99a1477a59f6f4683f3e8376393fad30185932a9
posita <posita@denshikousakubu.com>
2022-02-07 05:08:28 +0900

    Update：プロジェクト作成時

---
+++ CPP_BP/Source/CPP_BP/CPP_BPGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CPP_BPGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPP_BP_API ACPP_BPGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
