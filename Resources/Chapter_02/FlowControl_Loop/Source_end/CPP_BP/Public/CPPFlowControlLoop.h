d8c16e4d50144d7bac762952ec02e818debe8073
posita <posita@denshikousakubu.com>
2022-02-13 21:50:05 +0900

    New：C++ BP FlowControl(Loop)のプロジェクトとリソースを追加した

---
+++ Resources/Chapter_02/FlowControl_Loop/Source_end/CPP_BP/Public/CPPFlowControlLoop.h
@@ -0,0 +1,65 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CPPCalcType.h"
+#include "CPPFlowControlLoop.generated.h"
+
+UCLASS()
+class CPP_BP_API ACPPFlowControlLoop : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ACPPFlowControlLoop();
+
+	// Event Dispatcher[OnPrintHello]
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FPrintHelloDelegate);
+
+	UPROPERTY(BlueprintAssignable, Category = "CPP_BP")
+		FPrintHelloDelegate OnPrintHello;
+
+	// Custom Event[PrintHello] 
+	UFUNCTION()
+		void PrintHello();
+
+	int32 Sum(int32 A, int32 B);
+
+	// Action Mappingsに設定したActionを処理する関数
+	void PressedActionPrintCalcResult();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	TArray<FString> Messages = { TEXT("C++ Hello World!"), TEXT("你好 世界!"), TEXT("Bonjour le monde!"), TEXT("Hallo Welt!"), TEXT("こんにちは世界!") };
+	//TArray<FString> Messages = { "C++ Hello World!", "你好 世界!", "Bonjour le monde!", "Hallo Welt!", "こんにちは世界!" };
+
+	// 計算結果を出力する関数
+	void PrintCalcResult(const ECPPCalcType Type, const int32 A, const int32 B, const float PrintDuration);
+
+	// PrintString関数のDurationに設定する変数
+	const float Duration = 10.0f;
+
+	// PrintString関数のTextColorに設定する変数
+	const FLinearColor TextColor = FColor(255, 255, 255);
+
+	// 計算用の変数
+	int32 CalcVarA = 7;
+	int32 CalcVarB = 3;
+
+	// Flow Control用の変数
+	bool IsPrintHello = false;
+	int32 TypeIndex = 0;
+	TArray<ECPPCalcType> CalcTypes = { ECPPCalcType::Add, ECPPCalcType::Subtract, ECPPCalcType::Multiply, ECPPCalcType::Divide };
+
+	// Input設定
+	void SetupInput();
+
+	// Input Eventを処理する関数
+	void PressedH();
+
+};
---
+++ Resources/Chapter_02/FlowControl_Loop/Source_start/CPP_BP/Private/CPPFlowControlLoop.cpp
@@ -0,0 +1,112 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CPPFlowControlLoop.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Kismet/GameplayStatics.h"
+
+ACPPFlowControlLoop::ACPPFlowControlLoop()
+{
+	// Event Dispathcer[OnPrintHello]にCustom Event[PrintHello]をバインドする
+	OnPrintHello.AddDynamic(this, &ACPPFlowControlLoop::PrintHello);
+}
+
+int32 ACPPFlowControlLoop::Sum(int32 A, int32 B)
+{
+	return A + B;
+}
+
+// Called when the game starts or when spawned
+void ACPPFlowControlLoop::BeginPlay()
+{
+	SetupInput();
+
+	if (IsPrintHello)
+	{
+		// Hello World!を出力する処理
+		PrintHello();
+	}
+	else
+	{
+		// 計算結果を出力する処理
+		PressedActionPrintCalcResult();
+	}
+}
+
+void ACPPFlowControlLoop::PrintCalcResult(const ECPPCalcType Type, const int32 A, const int32 B, const float PrintDuration)
+{
+	switch (Type)
+	{
+		case ECPPCalcType::Add:
+		{
+			// Add(足し算)の処理
+			// 値渡し
+			int32 ResultAdd = Sum(A, B);
+			FString StrResultAdd = FString::Printf(TEXT("%d"), ResultAdd);
+			UKismetSystemLibrary::PrintString(this, StrResultAdd, true, true, FColor::Red, PrintDuration);
+			break;
+		}
+		case ECPPCalcType::Subtract:
+		{
+			// Subtract(引き算)の処理
+			int32 ResultSubtract = A - B;
+			FString StrResultSubtract = FString::Printf(TEXT("%d"), ResultSubtract);
+			UKismetSystemLibrary::PrintString(this, StrResultSubtract, true, true, FColor::Yellow, PrintDuration);
+			break;
+		}
+		case ECPPCalcType::Multiply:
+		{
+			// Multiply(掛け算)の処理
+			int32 ResultMultiply = A * B;
+			FString StrResultMultiply = FString::Printf(TEXT("%d"), ResultMultiply);
+			UKismetSystemLibrary::PrintString(this, StrResultMultiply, true, true, FColor::Green, PrintDuration);
+			break;
+		}
+		case ECPPCalcType::Divide:
+		{
+			// Divide(割り算)の処理(int > float)
+			float ResultDivide = (float)A / (float)B;
+			FString StrResultDivide = FString::Printf(TEXT("%f"), ResultDivide);
+			UKismetSystemLibrary::PrintString(this, StrResultDivide, true, true, FColor::Blue, PrintDuration);
+			break;
+		}
+	}
+}
+
+void ACPPFlowControlLoop::SetupInput()
+{
+	// 入力を有効にする
+	EnableInput(UGameplayStatics::GetPlayerController(GetWorld(), 0));
+
+	// HキーのPressedとReleasedをバインドする
+	InputComponent->BindKey(EKeys::H, IE_Pressed, this, &ACPPFlowControlLoop::PressedH);
+
+	// ActionMappingsに設定したActionをバインドする
+	InputComponent->BindAction("ActionPrintCalcResult", IE_Pressed, this, &ACPPFlowControlLoop::PressedActionPrintCalcResult);
+}
+
+void ACPPFlowControlLoop::PressedH()
+{
+	// Event Dispathcer[OnPrintHello]をコールする
+	OnPrintHello.Broadcast();
+}
+
+void ACPPFlowControlLoop::PressedActionPrintCalcResult()
+{
+	// 計算結果を出力する処理
+	PrintCalcResult(CalcTypes[TypeIndex], CalcVarA, CalcVarB, Duration);
+
+	TypeIndex++;
+	TypeIndex = TypeIndex % CalcTypes.Num();
+}
+
+void ACPPFlowControlLoop::PrintHello()
+{
+	int32 randomIndex = FMath::RandRange(0, Messages.Num() - 1);
+
+	// 1行で書くなら 
+	// Messages[FMath::RandRange(0, Messages.Num() - 1)]
+
+	// Hello World!を出力する処理
+	UKismetSystemLibrary::PrintString(this, Messages[randomIndex], true, true, TextColor, Duration);
+}
