a829e282fcdbe03ff357d34ec5c5cc1b5bbf5855
posita <posita@denshikousakubu.com>
2022-02-13 16:49:38 +0900

    New：C++ BP Enumerationのプロジェクトとリソースを追加した

---
+++ Resources/Chapter_02/Enumeration/Source_end/CPP_BP/Private/CPPFlowControlSwitchEnum.cpp
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CPPFlowControlSwitchEnum.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+// Called when the game starts or when spawned
+void ACPPFlowControlSwitchEnum::BeginPlay()
+{
+	FString Message = "C++ Hello World!";
+
+	if (IsPrintHello)
+	{
+		// PrintStringƒm[ƒh‚Æ“¯‚¶ˆ—
+		// UKismetSystemLibraryƒNƒ‰ƒX‚ÌPrintStringŠÖ”‚ðŒÄ‚Ño‚·
+		UKismetSystemLibrary::PrintString(this, Message, true, true, TextColor, Duration);
+	}
+	else
+	{
+		switch (CalcType)
+		{
+			case ECPPCalcType::Add:
+			{
+				// Add(‘«‚µŽZ)‚Ìˆ—
+				int32 ResultAdd = CalcVarA + CalcVarB;
+				FString StrResultAdd = FString::Printf(TEXT("%d"), ResultAdd);
+				UKismetSystemLibrary::PrintString(this, StrResultAdd, true, true, FColor::Red, Duration);
+				break;
+			}
+			case ECPPCalcType::Subtract:
+			{
+				// Subtract(ˆø‚«ŽZ)‚Ìˆ—
+				int32 ResultSubtract = CalcVarA - CalcVarB;
+				FString StrResultSubtract = FString::Printf(TEXT("%d"), ResultSubtract);
+				UKismetSystemLibrary::PrintString(this, StrResultSubtract, true, true, FColor::Yellow, Duration);
+				break;
+			}
+			case ECPPCalcType::Multiply:
+			{
+				// Multiply(Š|‚¯ŽZ)‚Ìˆ—
+				int32 ResultMultiply = CalcVarA * CalcVarB;
+				FString StrResultMultiply = FString::Printf(TEXT("%d"), ResultMultiply);
+				UKismetSystemLibrary::PrintString(this, StrResultMultiply, true, true, FColor::Green, Duration);
+				break;
+			}
+			case ECPPCalcType::Divide:
+			{
+				// Divide(Š„‚èŽZ)‚Ìˆ—(int > float)
+				float ResultDivide = (float)CalcVarA / (float)CalcVarB;
+				FString StrResultDivide = FString::Printf(TEXT("%f"), ResultDivide);
+				UKismetSystemLibrary::PrintString(this, StrResultDivide, true, true, FColor::Blue, Duration);
+			}
+		}
+	}
+}
